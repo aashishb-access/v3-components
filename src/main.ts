50c8d43834e50ebc67d3bfd42cf9c7ac62b4bac4
aashishb-access <aashish.b@access-designs.com>
2021-07-08 12:35:19 +0530

    tried oruga

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Oruga from '@oruga-ui/oruga-next'
+import '@oruga-ui/oruga-next/dist/oruga.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(Oruga).mount('#app')
